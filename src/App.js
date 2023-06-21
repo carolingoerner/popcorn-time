a8aa69ab0871e739d85fa0cdff4b9a0d43cd8d23
Carolin Görner <meins@MacBook-Air-von-Carolin.fritz.box>
2023-06-21 16:55:03 +0200

    code update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
+import { useState } from "react";
+import movies from "./data/movies.json";
+
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 
 import './App.css';
+import AddMovie from "./components/AddMovie";
 
 
 function App() {
-  
+
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+
+  const createMovie = (newMovie) => {
+    // new list = an array with the new movie + all the movies we had before
+    const newList = [newMovie, ...moviesToDisplay];
+
+    // update state
+    setMoviesToDisplay(newList);
+  }
+
+  const deleteMovie = (movieTitle) => {
+    console.log("deleting movie with id....", movieTitle)
+
+    //moviesToDisplay.push(); // NEVER MODIFY STATE DIRECTLY !
+
+    const newList = moviesToDisplay.filter((element) => {
+      return element.title !== movieTitle;
+    });
+
+    setMoviesToDisplay(newList);
+
+  }
+
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <AddMovie callbackToCreate={createMovie} />
+      <Main listOfMovies={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,63 +1,17 @@
-import { useState } from "react";
-import movies from "../data/movies.json";
+
+
 
 import "./Main.css";
+import Movie from "./Movie";
 
-function Main() {
-
-
-    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
-    
-
-    const deleteMovie = (movieId) => {
-        // console.log("deleting movie with id....", movieId)
-
-        // moviesToDisplay.push(); // NEVER MODIFY STATE DIRECTLY !
-
-        const newList = moviesToDisplay.filter( (element) => {
-            return element.id !== movieId;
-        });
-
-        setMoviesToDisplay(newList);
-
-    }
-
-
-    // Conditional Rendering: option A (Element Variables)
-    let message = "";
-    if(moviesToDisplay.length > 0){
-        message = <h1>Number of movies: {moviesToDisplay.length}</h1>;
-    } else {
-        message = <h1>Sorry, no movies to display</h1>;
-    }
-
-
+function Main(props) {
     return (
         <div className="Main">
 
-            {message}
-
-            {moviesToDisplay.map((movieObj) => {
-                return(
-                    <div key={movieObj.id} className="card">
-                        <p>{movieObj.title}</p>
-                        <p>Rating: {movieObj.rating}</p>
-
-
-                        { movieObj.imgURL 
-                            ? <img src={movieObj.imgURL} /> 
-                            : <img src="https://dummyimage.com/182x268/ffffff/000000" />
-                        }
-
-
-                        { movieObj.rating > 8 && <p>RECOMMENDED</p>}
-
-                        <p>
-                            <button onClick={ () => {deleteMovie(movieObj.id)}}>Delete this movie</button>
-                        </p>
-                    </div>
-                )
+            {props.listOfMovies.map((movieObj, index) => {
+                return <Movie key={index} movieDetails={movieObj} callbackToDelete={props.callbackToDelete} />
             })}
+
         </div>
     );
 }
